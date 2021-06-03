e30b4257721941a81a25b35739b13aaff1bfcf91
Haruka.K <72332745+haruponponpopon@users.noreply.github.com>
2021-06-04 07:46:19 +0900

    Create array.go

---
+++ golang/array.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var a [2]int
+	a[0] = 100
+	a[1] = 200
+	fmt.Println(a)
+
+	// var b [2]int{100, 200}
+	// b = append(b, 300) //エラー
+
+	var b []int = []int{100, 200}
+	b = append(b, 300)
+	fmt.Println(b)
+}
