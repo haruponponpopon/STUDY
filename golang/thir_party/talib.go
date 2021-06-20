207353eca6f1799985af20b051eaf5445dcfc9e1
Haruka.K <72332745+haruponponpopon@users.noreply.github.com>
2021-06-20 13:00:14 +0900

    Create talib.go

---
+++ golang/thir_party/talib.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/markcheno/go-quote"
+	"github.com/markcheno/go-talib"
+)
+
+func main() {
+	spy, _ := quote.NewQuoteFromYahoo("spy", "2021-03-01", "2021-03-31", quote.Daily, true)
+	fmt.Print(spy.CSV())
+	rsi2 := talib.Rsi(spy.Close, 2)
+	fmt.Println(rsi2)
+	mva := talib.Ema(spy.Close, 14)
+	fmt.Println(mva)
+}
